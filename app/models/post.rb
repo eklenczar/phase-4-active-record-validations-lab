94cf3c5b9af649ff4e7962b40dafc239a871aa76
Elizabeth Klenczar <eklenczar@gmail.com>
2023-05-07 23:59:19 -0400

    commit tree 9091bfb777cf62d6bfec2fb11db777ab593e5d6d

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,15 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: { minimum: 250 }
+    validates :summary, length: { maximum: 250 }
+    validates :category, inclusion: {in: ["Fiction", "Non-Fiction"]}
+    validate :is_clickbait
+
+    private
+
+    def is_clickbait
+        if title.present? && title.exclude?("Won't Believe" || "Secret" || "Top " || "Guess")
+            errors.add(:title, "Needs to be more clickbait-y!!!")
+        end
+    end
 end
